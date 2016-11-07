8e64a8c5831ca1c422592c797e996fe9cdaa77a5
jiaosiyuan785 <785220866@qq.com>
2016-11-07 17:38:47 +0800

    第二次备份

--- gridviewimage/src/main/java/com/example/gridviewimage/view/adapter/ImagePagerAdapter.java
+++ gridviewimage/src/main/java/com/example/gridviewimage/view/adapter/ImagePagerAdapter.java
@@ -1,24 +1,24 @@
 package com.example.gridviewimage.view.adapter;
 
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.support.v4.view.PagerAdapter;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 import android.widget.ProgressBar;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
-import com.bumptech.glide.load.resource.drawable.GlideDrawable;
 import com.bumptech.glide.request.animation.GlideAnimation;
-import com.bumptech.glide.request.target.GlideDrawableImageViewTarget;
+import com.bumptech.glide.request.target.SimpleTarget;
 import com.example.gridviewimage.R;
 import com.example.gridviewimage.view.acitvity.MaxPictureActivity;
+import com.example.gridviewimage.view.controls.TestImageView;
 
 import java.util.ArrayList;
 
 /**
- * Created by 焦思远 on 2016/9/6.
+ * Created by Kate on 2016/9/6.
  */
 public class ImagePagerAdapter extends PagerAdapter {
     ArrayList<View> viewContainter = new ArrayList<View>();
@@ -32,39 +32,42 @@ public class ImagePagerAdapter extends PagerAdapter {
         this.imageList = imgList;
     }
 
-    //viewpager中的组件数量
     @Override
     public int getCount() {
         return viewContainter.size();
     }
 
-    //滑动切换的时候销毁当前的组件
     @Override
     public void destroyItem(ViewGroup container, int position,
                             Object object) {
         container.removeView(viewContainter.get(position));
     }
 
-    //每次滑动的时候生成的组件
+    public static Bitmap convertViewToBitmap(View view) {
+        view.measure(View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED), View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED));
+        view.layout(0, 0, view.getMeasuredWidth(), view.getMeasuredHeight());
+        view.buildDrawingCache();
+        Bitmap bitmap = view.getDrawingCache();
+
+        return bitmap;
+    }
+
     @Override
     public Object instantiateItem(ViewGroup container, int position) {
         container.addView(viewContainter.get(position % viewContainter.size()), 0);
-        final ImageView imageView = (ImageView) ((MaxPictureActivity) context).findViewById(R.id.imageView);
+        final TestImageView imageView = (TestImageView) ((MaxPictureActivity) context).findViewById(R.id.imageView);
         final ProgressBar progressBar = (ProgressBar) ((MaxPictureActivity) context).findViewById(R.id.progressBar2);
-        imageView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                ((MaxPictureActivity) context).finish();
-            }
-        });
-        Glide.with(context).load(imageList.get(position)).diskCacheStrategy(DiskCacheStrategy.ALL).crossFade().thumbnail(0.1f).error(R.mipmap.image_error).into(
-                new GlideDrawableImageViewTarget(imageView) {
+        imageView.setClickable(true);
+        Glide.with(context).load(imageList.get(position)).asBitmap().diskCacheStrategy(DiskCacheStrategy.ALL).thumbnail(0.1f).error(R.mipmap.image_error).into(
+                new SimpleTarget<Bitmap>() {
                     @Override
-                    public void onResourceReady(GlideDrawable drawable, GlideAnimation<? super GlideDrawable> anim) {
-                        super.onResourceReady(drawable, anim);
+                    public void onResourceReady(Bitmap bitmap, GlideAnimation glideAnimation) {
+                        imageView.setImageBitmap(bitmap);
+                        imageView.imageBitmap(bitmap);
                         progressBar.setVisibility(View.GONE);
                     }
-                });
+                }
+        );
         return viewContainter.get(position % viewContainter.size());
     }
 
